fabd59eb1e415f52cc97b47837ad89cfe2d1031d
Radek Kotesovec <radek.kotesovec@onsemi.com>
2021-10-18 18:11:07 +0200

    deleted _site folder

--- _site/js/masonry_gallery.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var $masonry = $('#container').masonry({
-        itemSelector: '.item',
-        columnWidth: 200
-    });
-    $masonry.imagesLoaded().progress( function() {
-        $grid.masonry('layout');
-    });
-});
