cb6b48aa4a1a09e92dc2abca4686597b9160d45c
Radek Kotesovec <radek.kotesovec@onsemi.com>
2021-10-20 10:48:28 +0200

    added caching data into competitors while development

--- _data/competitors.js
+++ _data/competitors.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const https = require('https');
+const Cache = require("@11ty/eleventy-cache-assets");
 
 const httpsAgent = new https.Agent({
   rejectUnauthorized: false,
@@ -7,6 +8,18 @@ const httpsAgent = new https.Agent({
 
 
 module.exports = async function() {
+  // let json = await Cache("http://localhost:13124/get_all_competitors", {
+  let json = await Cache("https://rest.dogtrekking.cz/get_all_competitors", {
+    duration: "1d", // 1 day
+    type: "json" // also supports "text" or "buffer"
+  });
+
+  return {
+    competitors: json
+  };
+
+
+  /*
     //return fetch("http://localhost:13124/get_all_competitors")
     return fetch("https://rest.dogtrekking.cz/get_all_competitors", {
         method: 'GET',
@@ -18,4 +31,5 @@ module.exports = async function() {
                     competitors: json
                 };
             });
+  */
 };
