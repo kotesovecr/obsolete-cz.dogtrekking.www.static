cd0be9d922ab78fe31a3d8cc3c81901f556ecc81
Radek Kotesovec <radek.kotesovec@dogtrekking.cz>
2021-12-24 10:58:41 +0100

    fixed type

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("js");
-  eleventzConfig.addPassthroughCopy("docs");
+  eleventyConfig.addPassthroughCopy("docs");
 
   eleventyConfig.addPlugin(syntaxHighlight, {
     lineSeparator: "\n"
